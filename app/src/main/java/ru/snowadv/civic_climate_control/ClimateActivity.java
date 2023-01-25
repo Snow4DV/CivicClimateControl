4a7103e77afbfd75e320d703d589a9785d8e3903
snowadv <log.mihail2003@gmail.com>
2023-01-25 15:47:41 +0300

    Bindings

--- app/src/main/java/ru/snowadv/civic_climate_control/ClimateActivity.java
+++ app/src/main/java/ru/snowadv/civic_climate_control/ClimateActivity.java
@@ -1,14 +1,53 @@
 package ru.snowadv.civic_climate_control;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import java.util.Objects;
+
+import ru.snowadv.civic_climate_control.databinding.ActivityClimateBinding;
 
 public class ClimateActivity extends AppCompatActivity {
+
+
+    private ActivityClimateBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_climate);
+
+        hideTitleAndNotificationBars();
+
+        View rootView = initViewBinding();
+
+        setContentView(rootView);
+        initInterfaceListeners();
+    }
+
+    @NonNull
+    private View initViewBinding() {
+        binding = ActivityClimateBinding.inflate(getLayoutInflater());
+        return binding.getRoot();
+    }
+
+
+    private void initInterfaceListeners() {
+        binding.settingsButton.setOnClickListener(view -> openSettingsActivity());
+    }
+
+    private void hideTitleAndNotificationBars() {
+        Objects.requireNonNull(getSupportActionBar()).hide();
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+
+    private void openSettingsActivity() {
+        Intent settingsIntent = new Intent(ClimateActivity.this, SettingsActivity.class);
+        startActivity(settingsIntent);
     }
 }
--- app/src/main/java/ru/snowadv/civic_climate_control/SettingsActivity.java
+++ app/src/main/java/ru/snowadv/civic_climate_control/SettingsActivity.java
@@ -1,17 +1,27 @@
 package ru.snowadv.civic_climate_control;
 
+import android.content.Context;
+import android.hardware.usb.UsbManager;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.PreferenceFragmentCompat;
 
+import ru.snowadv.civic_climate_control.databinding.SettingsActivityBinding;
+
 public class SettingsActivity extends AppCompatActivity {
+
+    private SettingsActivityBinding binding;
+    private UsbManager usbManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.settings_activity);
+        usbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
+        setContentView(initBinding());
         if (savedInstanceState == null) {
             getSupportFragmentManager()
                     .beginTransaction()
@@ -22,6 +32,28 @@ public class SettingsActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+    }
+
+
+    private void initUsbDevices() {
+
+    }
+
+
+    private View initBinding() {
+        binding = SettingsActivityBinding.inflate(getLayoutInflater());
+        return binding.getRoot();
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                super.onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public static class SettingsFragment extends PreferenceFragmentCompat {
